5f3af75916bc4ea83b7b8e4dd80fea822228dfb6
Tatsuya Matsuda <65066529+nanshiki548@users.noreply.github.com>
2023-11-18 23:31:46 +0900

    Delete apps/config.py

--- apps/config.py
+++
@@ -1,9 +0,0 @@
-openai_api_key = "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"
-spreadsheet_url = "https://docs.google.com/spreadsheets/d/1oBY6tSmwxsLod2kiULVNbQelUmgAWLWiFcGlxl_bRdw/edit#gid=0"
-json_keyfile_path = (
-    "/Users/matsudatatsuya/gpt_sample/voltaic-space-388911-7adfbc11d865.json"
-)
-scope = [
-    "https://spreadsheets.google.com/feeds",
-    "https://www.googleapis.com/auth/drive",
-]
